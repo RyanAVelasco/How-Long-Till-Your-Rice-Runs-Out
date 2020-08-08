0a82f85e43f54058a3391d5d98b1b19028f465e4
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2020-08-07 23:44:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from tkinter import *
 mainWindow = Tk()
 
 # === Create label widget
-bagWeight = Label(mainWindow, text="Bag weight: 8000g", pady=10).grid(row=0, padx=20)
-noPeople = Label(mainWindow, text="Number of people", pady=10).grid(row=2, padx=20)
-portion = Label(mainWindow, text="Portion size", pady=10).grid(row=4, padx=20)
+bagWeight = Label(mainWindow, text="Bag weight: 8kg", pady=5).grid(row=1, column=0, padx=10)
+noPeople = Label(mainWindow, text="Number of people", pady=5).grid(row=2, column=0, padx=10)
+portion = Label(mainWindow, text="Portion size", pady=5).grid(row=3, column=0, padx=10)
 
 # === Entry fields
 weight = Entry(mainWindow)
-weight.grid(row=1)
+weight.grid(row=1, column=1, padx=10)
 people = Entry(mainWindow)
-people.grid(row=3)
+people.grid(row=2, column=1, padx=10)
 serving = Entry(mainWindow)
-serving.grid(row=5)
+serving.grid(row=3, column=1, padx=10)
+
 
 def calculate():
     days = 1
@@ -29,20 +30,20 @@ def calculate():
     # print("Your rice will last:", days / 365, "years")
 
     days = days
-    weeks = days/52
-    years = days/365
+    weeks = days//52
+    years = days//365
 
     label_days = Label(mainWindow, text=("Days:", days))
     label_weeks = Label(mainWindow, text=("Weeks:", weeks))
     label_years = Label(mainWindow, text=("Years:", years))
 
-    label_days.grid(row=7)
-    label_weeks.grid(row=8)
-    label_years.grid(row=9)
+    label_days.grid(row=7, columnspan=2)
+    label_weeks.grid(row=8, columnspan=2)
+    label_years.grid(row=9, columnspan=2)
 
 # === Create button
-button = Button(mainWindow, text="CALCULATE", command=calculate, fg="blue")
-button.grid(row=6, pady=10)
+button = Button(mainWindow, text="CALCULATE", command=calculate, fg="black")
+button.grid(row=6, columnspan=2, column=0, pady=10)
 
 # === Main loop
 mainWindow.mainloop()
